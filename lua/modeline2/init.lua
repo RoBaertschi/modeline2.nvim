46b1370c1c69562942941f3a8e5ac93d2488cdbe
RoBaertschi <rtmbaertschi007@gmail.com>
2024-08-22 14:58:54 +0200

    added the parse action function

--- lua/modeline2/init.lua
+++ lua/modeline2/init.lua
@@ -1,6 +1,7 @@
-local M = {}
+local M = {
+	key_value_seperator = "=",
+}
 local strings = require("string")
-
 
 ---Checks if "string" contains "contains".
 ---@param string string
@@ -18,20 +19,123 @@ M.contains = function(string, contains)
 	return false, -1
 end
 
-string:contains = function (string, contains)
-	return M.contains(string, contains)
+---@class Config
+---@field key_value_seperator string  Default is "=", should only be one char long
+
+---@param config? Config
+M.setup = function(config)
+	if config == nil then
+		return
+	end
+
+	if config.key_value_seperator then
+		M.key_value_seperator = config.key_value_seperator
+	end
+end
+
+---@type string[]
+local actions = {
+	"filetype",
+	"textwidth",
+	"softtabstop",
+	"tabstop",
+	"shiftwidth",
+	"expandtab",
+	"noexpandtab",
+	"foldmethod",
+	"readonly",
+	"noreadonly",
+	"rightleft",
+	"norightleft",
+}
+
+---@type {[string]: string}
+local short_to_action_mapping = {
+	ft = "filetype",
+	tw = "textwidth",
+	sts = "softtabstop",
+	ts = "tabstop",
+	sw = "shiftwidth",
+	et = "expandtab",
+	noet = "noexpandtab",
+	fdm = "foldmethod",
+	ro = "readonly",
+	noro = "noreadonly",
+	rl = "rightleft",
+	norl = "norightleft",
+}
+
+---@type string[]
+local no_value_actions = {
+	"rightleft",
+	"norightleft",
+	"readonly",
+	"noreadonly",
+	"expandtab",
+	"noexpandtab",
+}
+
+---@alias ActionType "lua"|"filetype"|"textwidth"|"softtabstop"|"tabstop"|"shiftwidth"|"expandtab"|"noexpandtab"|"foldmethod"|"readonly"|"noreadonly"|"rightleft"|"norightleft"
+---@class Action
+---@field type ActionType
+---@filed value string
+
+--- parse "key=value "
+---@param string string
+---@param cur_pos integer
+---@return string, string?, integer
+M.parse_action = function(string, cur_pos)
+	local parsed_key = ""
+
+	local value = string:sub(cur_pos, cur_pos)
+	while not (string:sub(cur_pos, cur_pos) == M.key_value_seperator or string:sub(cur_pos, cur_pos) == " ") do
+		if #string <= cur_pos then
+			parsed_key = parsed_key .. string:sub(cur_pos, cur_pos)
+			return parsed_key, nil, cur_pos
+		end
+		parsed_key = parsed_key .. string:sub(cur_pos, cur_pos)
+		cur_pos = cur_pos + 1
+	end
+
+	if string:sub(cur_pos, cur_pos) == " " then
+		-- We have no value
+		return parsed_key, nil, cur_pos
+	end
+
+	local parsed_value = ""
+	cur_pos = cur_pos + 1
+
+	while not (string:sub(cur_pos, cur_pos) == " ") do
+		if #string <= cur_pos then
+			parsed_value = parsed_value .. string:sub(cur_pos, cur_pos)
+			break
+		end
+
+		parsed_value = parsed_value .. string:sub(cur_pos, cur_pos)
+		cur_pos = cur_pos + 1
+	end
+
+	return parsed_key, parsed_value, cur_pos
 end
 
 ---Determine if the provided string contains modeline2 settings.
 ---@param string string
----@return boolean
-M.is_modeline_string = function(string)
+---@return boolean, Action[]
+M.get_modeline_string = function(string)
 	local contained, pos = M.contains(string, "ml2")
-	if contained or pos == -1 then
-		return false
+	if not contained or pos == -1 then
+		return false, {}
 	end
 
-	return true
+	---@type Action[]
+	local found_actions = {}
+	local cur_pos = pos + #"ml2"
+
+	if cur_pos > #string then
+		return true, actions
+	end
+
+	return true, actions
 end
 
 return M
--- tests/modeline2.lua
+++ tests/modeline2.lua
@@ -1,3 +1,4 @@
+---@module "plenary"
 describe("utils", function()
 	local ml2 = require("modeline2")
 
@@ -12,5 +13,37 @@ describe("utils", function()
 		assert.is_false(ml2.contains(test, "llod"))
 		local _, pos = ml2.contains(test, "world")
 		assert.equals(pos, 7)
+	end)
+
+	it("M#parse_action key value with space", function()
+		local key_value = "test=test2 "
+		local key, value, pos = ml2.parse_action(key_value, 1)
+		assert.equals("test", key)
+		assert.equals("test2", value)
+		assert.equals(11, pos)
+	end)
+
+	it("M#parse_action key value without space", function()
+		local key_value = "test=test2"
+		local key, value, pos = ml2.parse_action(key_value, 1)
+		assert.equals("test", key)
+		assert.equals("test2", value)
+		assert.equals(10, pos)
+	end)
+
+	it("M#parse_action key with space", function()
+		local key_value = "test "
+		local key, value, pos = ml2.parse_action(key_value, 1)
+		assert.equals("test", key)
+		assert.equals(nil, value)
+		assert.equals(5, pos)
+	end)
+
+	it("M#parse_action key without space", function()
+		local key_value = "test"
+		local key, value, pos = ml2.parse_action(key_value, 1)
+		assert.equals("test", key)
+		assert.equals(nil, value)
+		assert.equals(4, pos)
 	end)
 end)
